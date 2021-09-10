fd29295df5a870b8afbb45b9da9c8b0d90f8f3fc
Vidoslav Kakuca <vidoslav_k_96@hotmail.com>
2021-09-10 16:05:57 +0200

    API - create mail configuration file

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,32 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { MailerModule } from '@nestjs-modules/mailer';
-import { join } from 'path';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import mailConfig from '../config/mail.config';
 
 @Module({
-  imports: [
-    MailerModule.forRoot({
-      transport: {
-        host: process.env.MAIL_HOST,
-        secure: Boolean(process.env.MAIL_SECURE),
-        auth: {
-          user: process.env.MAIL_USER,
-          pass: process.env.MAIL_PASSWORD,
-        },
-      },
-      defaults: {
-        from: process.env.MAIL_DEFAULT_FROM,
-      },
-      template: {
-        dir: join(__dirname, 'templates'),
-        adapter: new HandlebarsAdapter(),
-        options: {
-          strict: true,
-        },
-      },
-    }),
-  ],
+  imports: [MailerModule.forRoot(mailConfig)],
   providers: [MailService],
   exports: [MailService],
 })
